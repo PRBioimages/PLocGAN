8f4925462c7c0119431ae27f60e3b123fb4760bd
Pattern Recognition and Bioimage Informatics Group <84379335+PRBioimages@users.noreply.github.com>
2023-10-04 00:01:33 +0800

    Delete part1-PLocGAN directory

--- part1-PLocGAN/config.py
+++
@@ -1,7 +0,0 @@
-IMGSIZE = 256
-TrainLabels_h5 = '../data/trainlabels_CYNP.h5'
-ValLabels_h5 = '../data/vallabels_CYNP.h5'
-TestLabels_h5 = '../data/testlabels_CYNP.h5'
-TrainImgs_basedir = '../data/CYNP/train'
-ValImgs_basedir ='../data/CYNP/val'
-TestImgs_basedir ='../data/CYNP/test'
